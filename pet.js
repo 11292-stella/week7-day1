3530ec9d42fa1e5ef3d15ffc54b9504949b7d0da
11292-stella <stella.marucelli@gmail.com>
2025-03-17 15:25:42 +0100

    primo esercizio

--- pet.js
+++ pet.js
@@ -25,3 +25,28 @@ form.addEventListener("submit", function (e) {
 
   console.log(pet)
 })
+
+//primo esercizio
+
+class User {
+  constructor(_firstName, _lastName, _age, _location) {
+    this.firstName = _firstName
+    this.lastName = _lastName
+    this.age = _age
+    this.location = _location
+  }
+
+  compareAge(altroUser) {
+    if (this.age > altroUser.age) {
+      return `${this.firstName} è più vecchio di ${altroUser.firstName}`
+    } else {
+      return `${this.firstName} e ${altroUser.firstName}hanno la stessa età`
+    }
+  }
+}
+
+const user1 = new User("Alice", "Rossi", 30, "Milano")
+const user2 = new User("Bob", "Bianchi", 25, "Roma")
+
+console.log(user1.compareAge(user2))
+console.log(user2.compareAge(user1))
